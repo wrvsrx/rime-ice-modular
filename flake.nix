a9e15d66983cc69a40aaae948955b75fd4ebb2de
wrvsrx <wrvsrx@outlook.com>
2025-07-23 17:16:06 +0800

    feat: rename `rime-ice` to `rime-ice-modular`

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
                   source = {
                     pname = "rime-ice-modular";
                     src = ./.;
-                    version = "2025.04.26-03";
+                    version = "2025.04.26-04";
                   };
                 };
                 components' = import ./components.nix {
